996b9389b2d48796d975e609d54df508dbb80cf1
Code Inverness <99823868+RobFraserCodes@users.noreply.github.com>
2024-02-14 16:39:03 +0000

    initial commit

---
+++ config/site.ts
@@ -0,0 +1,20 @@
+import { SiteConfig, NavItem } from "@/types"
+
+export const siteConfig: SiteConfig = {
+  name: "Rob Fraser Dev",
+  description:
+    "An open source application built using the new router, server components and everything new in Next.js 13.",
+  url: "https://tx.shadcn.com",
+  ogImage: "https://tx.shadcn.com/og.jpg",
+  links: {
+    twitter: "https://twitter.com/shadcn",
+    github: "https://github.com/shadcn/taxonomy",
+  },
+}
+
+export const navigation = [
+  { title: "Home", href: "/" },
+  { title: "About", href: "/about" },
+  { title: "Services", href: "/services" },
+  { title: "Contact", href: "/contact" },
+]
