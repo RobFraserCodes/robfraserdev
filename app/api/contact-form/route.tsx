d5cec49de7ad82ebffc04674928735ab46625fe9
Code Inverness <99823868+RobFraserCodes@users.noreply.github.com>
2024-02-20 16:20:27 +0000

    Update route.tsx

--- app/api/contact-form/route.tsx
+++ app/api/contact-form/route.tsx
@@ -1,22 +1,31 @@
-import * as postmark from "postmark";
-const serverToken = process.env.POSTMARK_API_TOKEN;
+import client from '@/postmark'
 
 export async function POST(request: Request) {
     const res = await request.json();
-    let client = new postmark.ServerClient("8127b402-d9d8-410d-93d4-a2253d963a56");
     
-    client.sendEmail({
-        "From": "hi@robfraser.dev",
-        "To": "hi@robfraser.dev",
-        "Subject": "New Contact Form Submission",
-        "HtmlBody": `
-            <p>First Name: ${res.firstName}</p>
-            <p>Last Name: ${res.lastName}</p>
-            <p>Email: ${res.email}</p>
-            <p>Phone: ${res.phone}</p>
-            <p>Message: ${res.message}</p>
-        `,
-    });
+    try {
+        await client.sendEmail({
+            "From": "hi@robfraser.dev",
+            "To": "hi@robfraser.dev",
+            "Subject": "New Contact Form Submission",
+            "HtmlBody": `
+                <p>First Name: ${res.firstName}</p>
+                <p>Last Name: ${res.lastName}</p>
+                <p>Email: ${res.email}</p>
+                <p>Phone: ${res.phone}</p>
+                <p>Message: ${res.message}</p>
+            `,
+        });
+    } catch (error) {
+        console.error("Failed to send email:", error);
+        // Consider handling the error, e.g., by returning a 500 status code
+        return new Response(JSON.stringify({ error: "Failed to send email" }), {
+            status: 500,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+    }
 
     return new Response(JSON.stringify(res), {
         headers: {
