14c2bf1a3937d203fc0cc9af4a220e8eb4c85fdb
Code Inverness <99823868+RobFraserCodes@users.noreply.github.com>
2024-02-20 15:51:27 +0000

    Update route.tsx

--- app/api/contact-form/route.tsx
+++ app/api/contact-form/route.tsx
@@ -1,8 +1,9 @@
-import client from '@/postmark'
+import * as postmark from "postmark";
+const serverToken = process.env.POSTMARK_API_TOKEN;
 
 export async function POST(request: Request) {
     const res = await request.json();
-    console.log(res);
+    let client = new postmark.ServerClient("8127b402-d9d8-410d-93d4-a2253d963a56");
     
     client.sendEmail({
         "From": "hi@robfraser.dev",
